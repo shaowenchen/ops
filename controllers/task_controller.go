e71dbdccf921ea701d35bb694e230689f6d2120c
shaowenchen <mail@chenshaowen.com>
2024-06-18 17:09:45 +0800

    controller support set runtime image env

--- controllers/task_controller.go
+++ controllers/task_controller.go
@@ -90,8 +90,13 @@ func (r *TaskReconciler) Reconcile(ctx context.Context, req ctrl.Request) (resul
 		return ctrl.Result{}, err
 	}
 	// validate task
-	if t.Spec.RuntimeImage == "" {
-		t.Spec.RuntimeImage = constants.DefaultRuntimeImage
+	if t.Spec.RuntimeImage != "" {
+		defaultRuntimeImage := os.Getenv("DEFAULT_RUNTIME_IMAGE")
+		if defaultRuntimeImage != "" {
+			t.Spec.RuntimeImage = defaultRuntimeImage
+		} else {
+			t.Spec.RuntimeImage = constants.DefaultRuntimeImage
+		}
 	}
 	// validate typeRef
 	if t.Spec.TypeRef == "" && t.Spec.NodeName == constants.AnyMaster {
