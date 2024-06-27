f2c116fc53eb63c7ba2b9478a90a824a45b6c4d7
shaowenchen <mail@chenshaowen.com>
2024-06-27 10:01:15 +0800

    minor

--- pkg/copilot/prompt.go
+++ pkg/copilot/prompt.go
@@ -30,43 +30,33 @@ func GetParametersPrompt(tool openai.Tool) string {
 	if tool.Function.Parameters == nil {
 		return ""
 	}
-	var b strings.Builder
+	var funcDesc strings.Builder
+	funcDesc.WriteString(fmt.Sprintf("The %s function is used to %s.\n", tool.Function.Name, tool.Function.Description))
+	if len(tool.Function.Parameters.(jsonschema.Definition).Properties) >= 0 {
+		funcDesc.WriteString("It requires the following parameters:\n")
+	}
 	for k, v := range tool.Function.Parameters.(jsonschema.Definition).Properties {
-		b.WriteString(fmt.Sprintf("parameter: %s\n", k))
-		b.WriteString(fmt.Sprintf("parameter type: string\n"))
-		b.WriteString(fmt.Sprintf("description: %s\n", v.Description))
+		parmDesc := fmt.Sprintf("%s (string, required): %s\n", k, v.Description)
 		if len(v.Enum) > 0 {
-			b.WriteString(fmt.Sprintf("choice: %v\n", v.Enum))
+			parmDesc += ", Valid values: " + strings.Join(v.Enum, ", ")
 		}
-		b.WriteString("---\n")
+		funcDesc.WriteString(parmDesc)
 	}
 	outputScheme := map[string]string{}
 	for k, _ := range tool.Function.Parameters.(jsonschema.Definition).Properties {
 		outputScheme[k] = "need to extract from the input"
 	}
-	outputScoutputSchemeBytes, _ := json.Marshal(outputScheme)
-	return `#You are an AI assistant specialized in extracting parameters from text. Your task is to analyze the given text, identify and extract key parameters, and then output the results in JSON format.
+	outputSchemeBytes, _ := json.Marshal(outputScheme)
+	return `You are an expert in parameter extraction, and are good at accurately extracting appropriate slots and parameters from user input according to different function requirements. Please do the following:
+-understand < Workflow > and output as required.
+-understand the tool description, subfunction description and parameters provided.
 
-Please follow these guidelines:
+< Workflow >
+1. According to the following function definition, accurately extract the appropriate parameters from the user input, and pay attention to the data type of the parameters.
+two。. Please make sure that the parameters you extract strictly follow the definition of the function and contain only the information explicitly mentioned in the user's input.
 
-1. Carefully analyze the provided text and identify possible parameters and their values.
-3. Organize the extracted parameters into JSON format.
-4. If the value of a parameter is uncertain, use "" as its value.
-5. If no parameters are found in the text, return an empty JSON object.
-6. Do not add any explanations or additional text, only output the JSON object.
-7. Do not add any extra parameters excluding the fields in Output Example.
-8. Do not return a list of JSON objects, just return first JSON object.
-
-Example:
-Input: "Please list pod names in cluster 1 on node 1"
-Output:
-{
-  "nameRef": "cluster1",
-  "nodeName": "node1",
-  "typeRef": "cluster",
-}
-
-Now, please analyze the following text and extract parameters:
-` + b.String() + `
-# Output Example: ` + string(outputScoutputSchemeBytes)
+Function definition:
+` + funcDesc.String() + `
+The parsing result of all necessary parameters is (in JSON format):` +
+		string(outputSchemeBytes)
 }
