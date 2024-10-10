576acba5d4b10d5254ce3dfae00dabce43d8e559
shaowenchen <mail@chenshaowen.com>
2024-10-10 10:07:18 +0800

    minor

--- pkg/event/template.go
+++ pkg/event/template.go
@@ -2,41 +2,43 @@ package event
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
-func (e EventHost) GetUsageDiskTemplate(t time.Time) string {
-	return fmt.Sprintf("cluster: %s\nhost: %s\ndisk usage: %s\ntime: %s\n", e.Cluster, e.Status.Hostname, e.Status.DiskUsagePercent, t.Local().Format("2006-01-02 15:04:05"))
+func (e EventHost) GetDiskUsageAlertMessage(t time.Time) string {
+	formattedTime := t.Local().Format("2006-01-02 15:04:05")
+
+	return fmt.Sprintf(
+		`kind: disk usage alert
+cluster: %s
+host: %s
+value: %s
+action: clean disk
+time: %s
+`,
+		e.Cluster,
+		e.Status.Hostname,
+		e.Status.DiskUsagePercent,
+		formattedTime,
+	)
 }
 
-func (e EventCheck) GetAlertTemplate(t time.Time) string {
-	var result string
-	if e.Cluster != "" {
-		result += fmt.Sprintf("cluster: %s\n", e.Cluster)
+func (e EventCheck) GetAlertMessage(t time.Time) string {
+	var result strings.Builder
+	appendField := func(label, value string) {
+		if value != "" {
+			result.WriteString(fmt.Sprintf("%s: %s\n", label, value))
+		}
 	}
-	if e.Host != "" {
-		result += fmt.Sprintf("host: %s\n", e.Host)
-	}
-	if e.Kind != "" {
-		result += fmt.Sprintf("kind: %s\n", e.Kind)
-	}
-	if e.Threshold != "" {
-		result += fmt.Sprintf("threshold: %s\n", e.Threshold)
-	}
-	if e.Operator != "" {
-		result += fmt.Sprintf("operator: %s\n", e.Operator)
-	}
-	if e.Value != "" {
-		result += fmt.Sprintf("value: %s\n", e.Value)
-	}
-	if e.Status != "" {
-		result += fmt.Sprintf("status: %s\n", e.Status)
-	}
-	if e.Reason != "" {
-		result += fmt.Sprintf("reason: %s\n", e.Reason)
-	}
-
-	result += fmt.Sprintf("time: %s\n", t.Local().Format("2006-01-02 15:04:05"))
-
-	return result
+	appendField("cluster", e.Cluster)
+	appendField("host", e.Host)
+	appendField("kind", e.Kind)
+	appendField("threshold", e.Threshold)
+	appendField("operator", e.Operator)
+	appendField("value", e.Value)
+	appendField("status", e.Status)
+	appendField("reason", e.Reason)
+	result.WriteString(fmt.Sprintf("time: %s\n", t.Local().Format("2006-01-02 15:04:05")))
+	return result.String()
 }
