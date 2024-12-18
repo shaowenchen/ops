647d2c3a691566ed1decaa1b5f378aafdbe60a6e
shaowenchen <mail@chenshaowen.com>
2024-12-18 12:59:18 +0800

    fix multi eventhook

--- pkg/event/bus.go
+++ pkg/event/bus.go
@@ -22,46 +22,56 @@ type ProducerConsumerClient struct {
 }
 
 func (globalClient *GlobalEventBusClients) GetClient(endpoint string, subject string) (*ProducerConsumerClient, error) {
-	// get from cache
 	key := endpoint + subject
+
 	globalClient.Mutex.RLock()
 	clientP, ok := globalClient.Clients[key]
 	globalClient.Mutex.RUnlock()
-	if !ok {
-		// build producer
-		producerP, err := cenats.NewSender(endpoint, subject, cenats.NatsOptions())
-		if err != nil {
-			return nil, err
-		}
-		producerClient, err := cloudevents.NewClient(producerP)
-		if err != nil {
-			return nil, err
-		}
-		// build consumer
-		consumerP, err := cenats.NewConsumer(endpoint, subject, cenats.NatsOptions())
-		if err != nil {
-			return nil, err
-		}
-		consumerClient, err := cloudevents.NewClient(consumerP)
-		if err != nil {
-			return nil, err
-		}
-		// update cache
-		globalClient.Mutex.Lock()
-		defer globalClient.Mutex.Unlock()
-		if globalClient.Clients == nil {
-			globalClient.Clients = make(map[string]*ProducerConsumerClient)
-		}
-		globalClient.Clients[key] = &ProducerConsumerClient{
-			Producer: &producerClient,
-			Consumer: &consumerClient,
-		}
-		clientP = globalClient.Clients[key]
+
+	if ok {
+		return clientP, nil
 	}
-	return clientP, nil
+
+	globalClient.Mutex.Lock()
+	defer globalClient.Mutex.Unlock()
+
+	clientP, ok = globalClient.Clients[key]
+	if ok {
+		return clientP, nil
+	}
+
+	// build producer
+	producerP, err := cenats.NewSender(endpoint, subject, cenats.NatsOptions())
+	if err != nil {
+		return nil, err
+	}
+	producerClient, err := cloudevents.NewClient(producerP)
+	if err != nil {
+		return nil, err
+	}
+
+	// build consumer
+	consumerP, err := cenats.NewConsumer(endpoint, subject, cenats.NatsOptions())
+	if err != nil {
+		return nil, err
+	}
+	consumerClient, err := cloudevents.NewClient(consumerP)
+	if err != nil {
+		return nil, err
+	}
+
+	// update cache with the new client
+	globalClient.Clients[key] = &ProducerConsumerClient{
+		Producer: &producerClient,
+		Consumer: &consumerClient,
+	}
+
+	return globalClient.Clients[key], nil
 }
 
-var CurrentEventBusClient = &GlobalEventBusClients{}
+var CurrentEventBusClient = &GlobalEventBusClients{
+	Clients: make(map[string]*ProducerConsumerClient),
+}
 
 type EventBus struct {
 	Server  string
@@ -109,9 +119,7 @@ func (bus *EventBus) Subscribe(ctx context.Context, fn func(ctx context.Context,
 	if err != nil {
 		return err
 	}
-
 	client.ConsumerFuncs = append(client.ConsumerFuncs, fn)
-
 	receiverFn := func(event cloudevents.Event) {
 		for _, consumerFunc := range client.ConsumerFuncs {
 			consumerFunc(ctx, event)
