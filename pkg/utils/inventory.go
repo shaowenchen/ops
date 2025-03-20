b37f244eb1373805c58e2e5caa0eee3a6562af27
shaowenchen <mail@chenshaowen.com>
2025-03-20 13:35:13 +0800

    fix get error target

--- pkg/utils/inventory.go
+++ pkg/utils/inventory.go
@@ -13,7 +13,7 @@ func GetInventoryType(inventory string) string {
 		return constants.InventoryTypeKubernetes
 	}
 	_, err = GetInClusterConfig()
-	if err == nil && len(inventory) == 0 {
+	if err == nil && len(inventory) != 0 {
 		return constants.InventoryTypeKubernetes
 	}
 
