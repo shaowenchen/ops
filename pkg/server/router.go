56959580506f11072856ef171a8af950a80245a2
shaowenchen <mail@chenshaowen.com>
2025-04-16 16:12:50 +0800

    debug mcp sse

--- pkg/server/router.go
+++ pkg/server/router.go
@@ -70,6 +70,12 @@ func SetupRouter(r *gin.Engine) {
 	v1MCP := r.Group("/api/v1/mcp")
 	{
 		sseHandler := func(c *gin.Context) {
+			c.Writer.Header().Set("Content-Type", "text/event-stream")
+			c.Writer.Header().Set("Cache-Control", "no-cache")
+			c.Writer.Header().Set("Connection", "keep-alive")
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("X-Accel-Buffering", "no")
+			c.Writer.Header().Set("Transfer-Encoding", "chunked")
 			sseServer, _ := NewSingletonMCPServer("debug",
 				GlobalConfig.Copilot.OpsServer,
 				GlobalConfig.Copilot.OpsToken,
